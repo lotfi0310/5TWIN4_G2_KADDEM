438c3a62fc338426939bcf7d505e12e2628dda45
dorakadri <dora.kadri@esprit.tn>
2023-10-24 22:31:46 +0100

    my first unit test

---
+++ src/test/java/tn/esprit/spring/kaddem/controllers/ContratRestControllerTest.java
@@ -0,0 +1,65 @@
+package tn.esprit.spring.kaddem.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.entities.Specialite;
+import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.services.ContratServiceImpl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ContratRestControllerTest {
+    @InjectMocks
+    private ContratServiceImpl contratService;
+
+    @Mock
+    private ContratRepository contratRepository;
+
+
+
+    @Test
+    void getContrats() {
+    }
+
+    @Test
+    public void testRetrieveAllContrats() {
+
+        List<Contrat> expectedContrats = new ArrayList<>();
+        expectedContrats.add(new Contrat(new Date(), new Date(), Specialite.SECURITE, true, 1000));
+        expectedContrats.add(new Contrat(new Date(), new Date(), Specialite.IA, true, 2000));
+
+
+
+        Mockito.when(contratRepository.findAll()).thenReturn(expectedContrats);
+
+
+        List<Contrat> actualContrats = contratService.retrieveAllContrats();
+
+
+        assertEquals(expectedContrats, actualContrats);
+    }
+
+    @Test
+    void addContrat() {
+    }
+
+    @Test
+    void removeContrat() {
+    }
+
+    @Test
+    void updateContrat() {
+    }
+}
