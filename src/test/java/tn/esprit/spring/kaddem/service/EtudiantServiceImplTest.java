15366190fb39b84022967f6448b245a52a106c27
lotfi0310 <lotfilouiz@gmail.com>
2023-10-25 17:44:20 +0200

    junit test on etudiant service

---
+++ src/test/java/tn/esprit/spring/kaddem/service/EtudiantServiceImplTest.java
@@ -0,0 +1,70 @@
+package tn.esprit.spring.kaddem.service;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.repositories.DepartementRepository;
+import tn.esprit.spring.kaddem.repositories.EquipeRepository;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+import tn.esprit.spring.kaddem.services.EtudiantServiceImpl;
+
+public class EtudiantServiceImplTest {
+
+    @Mock
+    private EtudiantRepository etudiantRepository;
+
+    @Mock
+    private ContratRepository contratRepository;
+
+    @Mock
+    private EquipeRepository equipeRepository;
+
+    @Mock
+    private DepartementRepository departementRepository;
+
+    @InjectMocks
+    private EtudiantServiceImpl etudiantService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testRetrieveAllEtudiants() {
+        List<Etudiant> etudiants = new ArrayList<>();
+        // Ajouter des étudiants à la liste
+        when(etudiantRepository.findAll()).thenReturn(etudiants);
+
+        List<Etudiant> result = etudiantService.retrieveAllEtudiants();
+
+        assertEquals(etudiants, result);
+    }
+
+    @Test
+    public void testAddEtudiant() {
+        Etudiant etudiant = new Etudiant();
+        when(etudiantRepository.save(etudiant)).thenReturn(etudiant);
+
+        Etudiant result = etudiantService.addEtudiant(etudiant);
+
+        assertEquals(etudiant, result);
+    }
+
+    // Ajoutez d'autres tests pour les autres méthodes de EtudiantServiceImpl
+    // en suivant un schéma similaire.
+
+}
+
