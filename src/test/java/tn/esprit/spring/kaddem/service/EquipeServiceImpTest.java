f90fe14a22aa23452b13041367e705928d7941d4
AhmedBenGuebila <ahmed.esprit@esprit.tn>
2023-10-31 22:23:56 +0100

    test junit + sonarqube

--- src/test/java/tn/esprit/spring/kaddem/service/EquipeServiceImpTest.java
+++ src/test/java/tn/esprit/spring/kaddem/service/EquipeServiceImpTest.java
@@ -9,16 +9,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import org.mockito.internal.matchers.Null;
+import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.kaddem.entities.Equipe;
+import tn.esprit.spring.kaddem.entities.Niveau;
 import tn.esprit.spring.kaddem.repositories.ContratRepository;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.repositories.EquipeRepository;
 import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
 import tn.esprit.spring.kaddem.services.EquipeServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
 public class EquipeServiceImpTest {
 
     @Mock
@@ -50,7 +56,25 @@ public class EquipeServiceImpTest {
 
         assertEquals(equipes, result);
     }
+    @Test
+    public void testAddEquipe() {
+        // Create a sample Equipe object for testing
 
+        Equipe equipe = new Equipe(1, "Test Equipe", Niveau.EXPERT, null, null);
+
+        // Mock the service method to return the sample Equipe
+        when(equipeService.addEquipe(equipe)).thenReturn(equipe);
+
+        // Call the controller method
+        Equipe result = equipeRepository.save(equipe);
+
+        // Verify that the service method was called with the correct Equipe object
+        verify(equipeService).addEquipe(equipe);
+
+        // Assert the result
+        assertEquals(equipe, result);
+    }
+}
 
 
 
